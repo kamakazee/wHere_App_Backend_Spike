c412f994c4278f3f33599ee30799d39f999980ce
Your Name <you@example.com>
2022-11-16 11:09:04 +0000

    add

--- schema/models.js
+++ schema/models.js
@@ -12,52 +12,57 @@ const UserSchema = new mongoose.Schema({
 });
 
 const ElementSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    isContainer: {
-      type: Boolean,
-      default: true,
-    },
-    imageUrl:{ 
-        type: String,
-        default:"defaultImage"},
+  name: {
+    type: String,
+    required: true,
+  },
+  isContainer: {
+    type: Boolean,
+    default: true,
+  },
+  imageUrl: {
+    type: String,
+    default: "defaultImage",
+  },
 
-        contains:{
-          type: Array,
-            default:"defaultImage"}
-  });
-
-
-  const ItemSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    isContainer: {
-      type: Boolean,
-      default: true,
-    },
-    imageUrl:{ 
-        type: String,
-        default:"defaultImage"},
-  });
-
-  var imageSchema = new mongoose.Schema({
-    name: String,
-    desc: String,
-    img:
-    {
-        data: Buffer,
-        contentType: String
-    }
+  contains: {
+    type: Array,
+    default: "defaultImage",
+  },
 });
 
+const ItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  isContainer: {
+    type: Boolean,
+    default: true,
+  },
+  imageUrl: {
+    type: String,
+    default: "defaultImage",
+  },
+});
+
+var imageSchema = new mongoose.Schema({
+  name: String,
+  desc: String,
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
+});
 
 const Image = mongoose.model("Image", imageSchema);
 const User = mongoose.model("User", UserSchema);
 const Element = mongoose.model("Element", ElementSchema);
-const Item =  mongoose.model("Item", ItemSchema);
+const Item = mongoose.model("Item", ItemSchema);
 
-module.exports = {userModel: User, elementModel: Element, itemModel: Item, imageModel: Image};
+module.exports = {
+  userModel: User,
+  elementModel: Element,
+  itemModel: Item,
+  imageModel: Image,
+};
