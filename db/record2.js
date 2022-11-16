8c27174d6a5c7f566d02403c0bf399e76cb9bf3c
Your Name <you@example.com>
2022-11-16 11:04:36 +0000

    refactor code

--- db/record2.js
+++ db/record2.js
@@ -1,20 +1,25 @@
 const express = require("express");
-const multer =require('multer')
-const fs = require('fs')
-const path = require('path');
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
-const {userModel, elementModel, itemModel, imageModel} = require("../schema/models.js")
+const {
+  userModel,
+  elementModel,
+  itemModel,
+  imageModel,
+} = require("../schema/models.js");
 
 //Code related to file upload---------------------------------------
 //------------------------------------------------------------------
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, 'uploads')
+    cb(null, "uploads");
   },
   filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now())
-  }
+    cb(null, file.fieldname + "-" + Date.now());
+  },
 });
 
 var upload = multer({ storage: storage });
@@ -25,126 +30,122 @@ var upload = multer({ storage: storage });
 const app = express();
 
 app.post("/add_user", async (request, response) => {
-    const user = new userModel(request.body);
+  const user = new userModel(request.body);
 
-    console.log("request body", request.body)
-    console.log("user", user)
-  
-    try {
-      await user.save();
-      response.send(user);
-    } catch (error) {
-      response.status(500).send(error);
-    }
-});
+  console.log("request body", request.body);
+  console.log("user", user);
 
-app.post("/add_element", async (request, response) => {
-    const element = new elementModel(request.body);
-
-    console.log("request body", request.body)
-    console.log("element", element)
-  
-    try {
-      await element.save();
-      response.send(element);
-    } catch (error) {
-      response.status(500).send(error);
-    }
-});
-
-app.get("/users", async (request, response) => {
-
-  const users = await userModel.find({});
-  
-    try {
-      response.send(users);
-    } catch (error) {
-      response.status(500).send(error);
-    }
-  });
-
-  app.post("/add_item", async (request, response) =>{
-
-    const item = new itemModel(request.body);
-
-    try {
-      await item.save();
-      response.send(item);
-      await elementModel.findOneAndUpdate({ _id: "637371904e79b2a678b82078" }, {$push:{ contains: item }});
-      console.log(item)
-
-    } catch (error) {
-      response.status(500).send(error);
-    }
-  })
-
-app.get("/items/:name", async (request, response) => {
-
-  const getItemById = await itemModel.find( {name: request.params.name} );
   try {
-    response.send(getItemById);
-    console.log("Inside of Item Get")
-    console.log(request.params.name)
-    console.log(getItemById)
+    await user.save();
+    response.send(user);
   } catch (error) {
     response.status(500).send(error);
   }
-    
-})
+});
 
-app.get('/images', (req, res) => {
+app.post("/add_element", async (request, response) => {
+  const element = new elementModel(request.body);
+
+  console.log("request body", request.body);
+  console.log("element", element);
+
+  try {
+    await element.save();
+    response.send(element);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.get("/users", async (request, response) => {
+  const users = await userModel.find({});
+
+  try {
+    response.send(users);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.post("/add_item", async (request, response) => {
+  const item = new itemModel(request.body);
+
+  try {
+    await item.save();
+    response.send(item);
+    await elementModel.findOneAndUpdate(
+      { _id: "637371904e79b2a678b82078" },
+      { $push: { contains: item } }
+    );
+    console.log(item);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.get("/items/:name", async (request, response) => {
+  const getItemById = await itemModel.find({ name: request.params.name });
+  try {
+    response.send(getItemById);
+    console.log("Inside of Item Get");
+    console.log(request.params.name);
+    console.log(getItemById);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.get("/images", (req, res) => {
   //console.log("Inside of GET")
 
-  imageModel.find({}, (err, items) => {           //find({}
-      if (err) {
-          console.log(err);
-          res.status(500).send('An error occurred', err);
-      }
-      else {
+  imageModel.find({}, (err, items) => {
+    //find({}
+    if (err) {
+      console.log(err);
+      res.status(500).send("An error occurred", err);
+    } else {
+      const imagesArray = [];
 
-         const imagesArray = []       
+      items.forEach((imageDoc) => {
+        const imageObject = {};
 
-          items.forEach((imageDoc)=>{
+        const imageToBase64String = imageDoc.img.data.toString("base64");
 
-            const imageObject= {}
-            
-            const imageToBase64String = imageDoc.img.data.toString('base64')
+        imageObject["name"] = imageDoc._doc.name;
+        imageObject["desc"] = imageDoc._doc.desc;
+        imageObject["_id"] = imageDoc._doc._id;
+        imageObject["img"] = imageToBase64String;
+        imagesArray.push(imageObject);
+      });
 
-            imageObject["name"] = imageDoc._doc.name
-            imageObject["desc"] = imageDoc._doc.desc
-            imageObject["_id"] = imageDoc._doc._id
-            imageObject["img"] = imageToBase64String
-            imagesArray.push(imageObject)
-          })
+      //console.log(imagesArray[0])
 
-          //console.log(imagesArray[0])
-
-          res.send(imagesArray)
-      }
+      res.send(imagesArray);
+    }
   });
 });
 
-app.post('/image', upload.single('file'), (req, res, next) => {
-  
+app.post("/image", upload.single("file"), (req, res, next) => {
   const obj = {
-      name: req.body.name,
-      desc: req.body.desc,
-      img: {
-          data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-          contentType: 'image/png'
-      }
-  }
+    name: req.body.name,
+    desc: req.body.desc,
+    img: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/uploads/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
 
   imageModel.create(obj, (err, item) => {
-      if (err) {
-          console.log(err);
-      }
-      else {
-          // item.save();
-          console.log("Id of uploaded image", item._id)
-          res.redirect('/');
-      }
+    if (err) {
+      console.log(err);
+    } else {
+      // item.save();
+      console.log("Id of uploaded image", item._id);
+      res.redirect("/");
+    }
   });
 });
 
-module.exports =  app;
+module.exports = app;
